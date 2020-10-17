dbf68f39931949e2073b90d4ea0b1820d38cfe9d
jobejoseph <65123975+jobejoseph@users.noreply.github.com>
2020-10-17 14:35:32 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "javahome_vpc" {
-  cidr_block     = "192.168.0.0.24"
-#   instance_tenancy = "default"
-  #tags = {
-   # Name = "${terraform.workspace}-javahome-vpc"
-  #}
+  cidr_block     = "192.168.0.0/24"
+  instance_tenancy = "default"
+  tags = {
+    Name = "demo-vpc"
+  }
 }
